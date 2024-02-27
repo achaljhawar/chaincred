3896eaf1021cd3280ae210204b8adfcfef04894a
achaljhawar <achaljhawar03@gmail.com>
2024-02-28 04:11:30 +0530

    fix

--- pages/api/verifydegree.js
+++ pages/api/verifydegree.js
@@ -19,7 +19,9 @@ async function handler(req, res) {
         const hasHash = await contract.hasHash(hash);
         if (!hasHash) {
           return res.status(400).json({ message: 'this degree is manipulated' });
-        } 
+        } else {
+          return res.status(200).json({ message: 'degree verified' });
+        }
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'An error occurred' });
